031b1988b5277e115a7b9ecb69d4eba2c8460674
sujaybalaji <sujayakon@gmail.com>
2024-06-11 16:07:57 +0530

    [development] added styles

--- src/components/profileData.js
+++ src/components/profileData.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { List, Col, Flex, Pagination } from "antd";
+import { List, Col, Flex, Pagination, Card, Typography } from "antd";
 import SearchApp from "./search";
 import { addData } from "../redux/common/commonSlice";
 import axios from "axios";
@@ -15,15 +15,19 @@ function ProfileData() {
   const dispatch = useDispatch();
   const profileDetail = useSelector((state) => state.common);
   // const [hasMore, setHasMore] = useState(true)
-  const [count, setCount] = useState(0)
-  const [data, setData] = useState()
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState();
 
-  const getSetData = () =>  profileDetail.searchedData ?? profileDetail.selectedData ?? profileDetail.data ?? []
+  const getSetData = () =>
+    profileDetail.searchedData ??
+    profileDetail.selectedData ??
+    profileDetail.data ??
+    [];
 
   const handelInfiniteScroll = () => {
-    const tempData = getSetData().slice(count, count + 20)
-    setData([...data, ...tempData])
-  }
+    const tempData = getSetData().slice(count, count + 20);
+    setData([...data, ...tempData]);
+  };
 
   const fetchData = async () => {
     try {
@@ -34,38 +38,61 @@ function ProfileData() {
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   useEffect(() => {
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   useEffect(() => {
     if (profileDetail) {
-      const tempData = getSetData()
-      setCount(0)
-      setData(tempData.slice(0, 20))
+      const tempData = getSetData();
+      setCount(0);
+      setData(tempData.slice(0, 20));
     }
-  } ,[profileDetail])
+  }, [profileDetail]);
 
   useEffect(() => {
     if (data) {
-      setCount(count + 20)
+      setCount(count + 20);
     }
-  }, [data])
+  }, [data]);
 
   return (
-    <>
-      <SearchApp />
-      <SelectUser />
-      <div style={{ marginTop: "5px" }}>
+    <Card bodyStyle={{ paddingTop: 0 }}>
+      <div
+        style={{
+          position: "sticky",
+          top: 3,
+          zIndex: 2000,
+          backgroundColor: "white",
+          padding: '10p 0'
+        }}
+      >
+        <Typography.Title level={4}>Profile Mangement</Typography.Title>
+        <div style={{ margin: "15px 0px" }}>
+          <SearchApp />
+          <SelectUser />
+        </div>
+      </div>
+      <div>
         {data && (
-          <InfiniteScroll dataLength={data.length} next={handelInfiniteScroll} hasMore>
+          <InfiniteScroll
+            dataLength={data.length}
+            next={handelInfiniteScroll}
+            hasMore
+          >
             <List
+              header={
+                <Flex gap={35}>
+                  <Typography.Text strong>Id</Typography.Text>
+                  <Typography.Text strong>Title</Typography.Text>
+                </Flex>
+              }
               dataSource={data}
               renderItem={(data) => (
                 <List.Item>
-                  <Flex gap={20}>
+                  <Flex gap={40}>
                     <div>{data?.id}</div>
                     <div>{data?.title}</div>
                   </Flex>
@@ -75,7 +102,7 @@ function ProfileData() {
           </InfiniteScroll>
         )}
       </div>
-    </>
+    </Card>
   );
 }
 
